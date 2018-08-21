189f57f98ea29c3d7faff09491985da3c5eebcba
Josue Garcia <JosueG@Magenic.com>
2018-08-20 20:31:26 -0500

    mock service calls using spys example

--- src/app/modules/email-search/email-typeahead.component.spec.ts
+++ src/app/modules/email-search/email-typeahead.component.spec.ts
@@ -3,11 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { EmailTypeaheadComponent } from './email-typeahead.component';
 import { EmailApiService } from './email-api.service';
+import { Observable } from 'rxjs';
 
 describe('EmailTypeaheadComponent', () => {
+  let service: EmailApiService;
   let component: EmailTypeaheadComponent;
   let fixture: ComponentFixture<EmailTypeaheadComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule, HttpClientModule],
@@ -18,6 +19,7 @@ describe('EmailTypeaheadComponent', () => {
   }));
 
   beforeEach(() => {
+    service = TestBed.get(EmailApiService);
     fixture = TestBed.createComponent(EmailTypeaheadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -25,5 +27,17 @@ describe('EmailTypeaheadComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('api returns results', () => {
+    component.searchValue = 'Josh';
+    spyOn(service, 'searchByName').and.callFake(() => {
+      return Observable.create(observer => {
+        observer.next({ users: [{ email: 'email@domain.com' }, { email: 'email2@domain.com' }] });
+      });
+    });
+    component.search();
+    expect(service.searchByName).toHaveBeenCalled();
+    expect(component.searchResults.length).toBe(2);
   });
 });
